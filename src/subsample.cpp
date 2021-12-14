868bcc6d260cc478aa8c2f45b820d8aff5d7ecf8
Gabriel Raulet <gabe.h.raulet@gmail.com>
2021-12-13 16:56:10 -0800

    install

--- src/subsample.cpp
+++ src/subsample.cpp
@@ -15,7 +15,7 @@ int main(int argc, char *argv[])
      *     "seqtk seq -l0 original.fa > in.fa"
      */
     if (argc != 3) {
-        std::cerr << "usage: ./subsample <in.fa> <idmap.tsv>" << std::endl;
+        std::cerr << "usage: subsample <in.fa> <idmap.tsv>" << std::endl;
         return -1;
     }
 
